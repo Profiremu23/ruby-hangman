de6f25b3d072eeb0d3ce447f2062f0d4ca5f71ef
Profiremu23 <profirdani23@gmail.com>
2024-04-15 20:17:45 +0200

    Added functional guessing (for a singular letter though), I will add additional outcomes later

--- hangman.rb
+++ hangman.rb
@@ -1,11 +1,26 @@
+# frozen_string_literal: true
+
 contents = File.open('google-10000-english-no-swears.txt')
 word_list = []
 
 contents.each do |word|
-  if word.length > 4 && word.length < 13
+  if word.length > 5 && word.length < 12
     word_list << word
   end
 end
+
 puts guess_word = word_list.sample
-puts guessable_word = guess_word.strip.split(//)
-puts guessable_word.class
+p guessable_word = guess_word.strip.split(//)
+p input = Array.new(guessable_word.size, '_')
+guess = gets.chomp
+
+if guessable_word.include?(guess)
+  replace = guessable_word.map.with_index { |x, i| i if x == guess }
+  replace.compact!
+  replace.each do |i|
+    p replace
+    input[i] = guess
+  end
+end
+
+p input
