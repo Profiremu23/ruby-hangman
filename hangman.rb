3dd340696ac28ee54cca4bc09760b3049c7fe885
Profiremu23 <profirdani23@gmail.com>
2024-04-16 20:12:57 +0200

    Reorganized the code structure into a singular game class, also added a mistakes counter for guess checks

--- hangman.rb
+++ hangman.rb
@@ -1,26 +1,53 @@
 # frozen_string_literal: true
 
-contents = File.open('google-10000-english-no-swears.txt')
-word_list = []
+# The game class for Hangman
+class GameClass
+  def initialize
+    @contents = File.open('google-10000-english-no-swears.txt')
+    @guess = ''
+    @guessable_word = ''
+    @input = []
+    @mistakes = 0
+    @used_letters = []
+    @word_list = []
+  end
 
-contents.each do |word|
-  if word.length > 5 && word.length < 12
-    word_list << word
+  def generate_guessable
+    @contents.each do |word|
+      @word_list << word if word.length > 5 && word.length < 12
+    end
+  end
+
+  def break_guessable
+    puts guess_word = @word_list.sample
+    p @guessable_word = guess_word.strip.split(//)
+    p @input = Array.new(@guessable_word.size, '_')
+    @guess = gets.chomp
+    @used_letters << @guess
+  end
+
+  def guess_check
+    if @guessable_word.include?(@guess)
+      replace = @guessable_word.map.with_index { |x, i| i if x == @guess }
+      replace.compact!
+      replace.each do |i|
+        @input[i] = @guess
+      end
+    else
+      @mistakes += 1
+    end
+    p @input
+    p @used_letters
+    p @mistakes
+  end
+
+  def hangman
+    generate_guessable
+    break_guessable
+    guess_check
   end
 end
 
-puts guess_word = word_list.sample
-p guessable_word = guess_word.strip.split(//)
-p input = Array.new(guessable_word.size, '_')
-guess = gets.chomp
-
-if guessable_word.include?(guess)
-  replace = guessable_word.map.with_index { |x, i| i if x == guess }
-  replace.compact!
-  replace.each do |i|
-    p replace
-    input[i] = guess
-  end
-end
-
-p input
+# Running the game
+hangman = GameClass.new
+hangman.hangman
